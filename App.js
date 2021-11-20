3d5627000bd5fbb6ea4a15cbfa5d4d6330581bbb
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-20 14:47:11 +0100

    flatlist

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
-  // I used the index argument given by the map instead of the keys HermesInternal, both works
   const [people, setPeople] = useState([
     { name: 'shaun', id: '1' },
     { name: 'yoshi', id: '2' },
@@ -13,19 +12,19 @@ export default function App() {
     { name: 'bowser', id: '7' },
   ]);
 
-
   return (
     <View style={styles.container}>
-      {/* when scrollview is not wrapped it dones't even scroll, weird */}
-      <ScrollView>
-        {
-          people.map((item, i) => (
-            <View key={i}>
-              <Text style={styles.item}>{item.name}, {i}</Text>
-            </View>
-          ))
-        }
-      </ScrollView>
+      {/* This is alot better for perfomrance when loading larger lists because it renders as the user scrolls */}
+      <FlatList 
+        numColumns={2}
+        // FlatList automatically gets the key, but if there aren't keys given,  i can define the keys like this
+        keyExtractor={(item) => item.id} 
+        data={people} 
+        renderItem={({ item }) => ( 
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
     </View>
   );
 }
@@ -33,14 +32,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  item:{
-    marginTop: 34,
-    padding: 20,
-    backgroundColor: 'red',
-    fontSize: 24
-  }
+  item: {
+    // with flex they take up all the available space
+    flex: 1,
+    // gave a margin in between two objects, cool 
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  },
 });

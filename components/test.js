85a005441ab0ae275eb60fa07fa68e2ef7289d75
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-20 15:32:54 +0100

    tested shorthand

---
+++ components/test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+// created with "rnfes", to test
+
+const test = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default test
+
+const styles = StyleSheet.create({})

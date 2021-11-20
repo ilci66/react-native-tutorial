613a681dcde95ed40067c35b8643f3614d7f1ecf
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-20 14:06:27 +0100

    scrolling logic

--- App.js
+++ App.js
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Ilker');
-  const [age, setAge] = useState('27');
+  // I used the index argument given by the map instead of the keys HermesInternal, both works
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
+
 
   return (
     <View style={styles.container}>
-      {/* the text acts like the container element here for the text inputs */}
-      <Text>Enter name:</Text>
-      {/* This acts like the form element, without any custiomization appears as blank space  */}
-      <TextInput 
-        placeholder='e.g. John Doe' 
-        style={styles.input}
-        // There is no event but they will still take value
-        onChangeText={(value) => setName(value)} />
-
-      <Text>Enter age:</Text>
-      <TextInput 
-        placeholder='e.g. 99' 
-        style={styles.input}
-        // well I only want them to enter numbers here so
-        keyboardType="numeric"
-        onChangeText={(value) => setAge(value)} />
-
-      <Text style={styles.result}>name: {name}, age: {age}</Text>
+      {/* when scrollview is not wrapped it dones't even scroll, weird */}
+      <ScrollView>
+        {
+          people.map((item, i) => (
+            <View key={i}>
+              <Text style={styles.item}>{item.name}, {i}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -36,11 +37,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item:{
+    marginTop: 34,
+    padding: 20,
+    backgroundColor: 'red',
+    fontSize: 24
   }
 });

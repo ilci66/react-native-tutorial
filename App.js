02a2bfc435aee8f81616abc96c087abaaeeece4c
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-20 12:42:52 +0100

    notes added

--- App.js
+++ App.js
@@ -6,18 +6,35 @@ export default function App() {
   return (
     // view is like div
     <View style={styles.container}>
-      <Text>wassap!!</Text>
-      <Text>Hello !</Text>
-      <StatusBar style="auto" />
+      <View>
+        {/* they don't get style porperties from parent elements such as bold texts etc. so for each element I need to define them  */}
+        <Text style={styles.header}>this be the title!</Text>
+        {/* when there is a text component inside another text component then the child text inerits the style porperties */}
+        <Text style={styles.boldText}><Text>example  </Text>First Text</Text>
+        
+        <Text style={styles.boldText}>Second Text</Text>
+        <Text style={styles.boldText}>Third Text</Text>
+      </View>
+
+      {/* <StatusBar style="auto" /> */}
     </View>
-  );
-}
-// this is how react-native emulates the css
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      );
+    }
+    // this is how react-native emulates the css
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      header:{
+        backgroundColor: 'yellow',
+      },
+      boldText:{
+        fontWeight: 'bold',
+        backgroundColor: 'blue',
+        color: "white",
+        padding: 10
+      }
 });

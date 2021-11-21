c4e8a9434d7b5a89165914321cad6c9a5765f192
ilci66 <akbiyik.ilker7@gmail.com>
2021-11-21 12:48:44 +0100

    notes on alert

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1' },
     { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' }
+    { text: 'play on the switch', key: '3' },
   ]);
 
   const pressHandler = (key) => {
@@ -16,21 +17,43 @@ export default function App() {
     });
   };
 
+  // argument here automatically becomes the target value of the 
+  const submitHandler = (text) => {
+    if(text.length > 3){
+      setText('');
+      setTodos(prevTodos => {
+        return [
+          { text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      // this is how you add alerts, looks very decent, no need to create a div that would act as the alert
+      // the comas act as the breaks so after avery coma the text goes to the other line below 
+      Alert.alert('OOPS', 'Todo must be over 3 characters long', [
+        // these act like the options 
+        {text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        {/* add todo form */}
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
---
+++ components/addTodo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+
+export default function AddTodo({ submitHandler }) {
+  [text, setText] = useState('');
+
+  const changeHandler = (val) => {
+    setText(val);
+  };
+
+  return (
+    <View>
+      <TextInput 
+        style={styles.input} 
+        placeholder='new todo...'
+        // onChangeText={(val) => changeHandler(val)} 
+        // don't even need to give it an argument, does the same as the one above 
+        onChangeText={changeHandler} 
+        value={text} 
+      />
+    {/* the argument of the handler here is the target item's value, no need to include event and stuff */}
+      <Button color='coral' onPress={() => submitHandler(text)} title='add todo' />
+      {/* cannot really style the buttons but there are still small things that can be changed, like color in this example */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+});
